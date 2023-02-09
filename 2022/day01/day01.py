6f71f2fe053678e4ac8b0fe60c33d0ea8e9a25b6
thetarro <salvo.scellato@gmail.com>
2023-02-09 22:08:23 +0000

    formatter

--- 2022/day01/day01.py
+++ 2022/day01/day01.py
@@ -2,34 +2,36 @@ from typing import List, Sequence, Tuple
 
 
 def first_solution(lines: Sequence[str]) -> None:
-  max_calories: int = 0
-  current_calories: List[int] = []
+    max_calories: int = 0
+    current_calories: List[int] = []
 
-  for line in lines:
-    if line == '\n':
-      current_value = sum(current_calories)
-      if current_value > max_calories:
-        max_calories = current_value
-      current_calories = []
-    else:
-      current_calories.append(int(line))
+    for line in lines:
+        if line == "\n":
+            current_value = sum(current_calories)
+            if current_value > max_calories:
+                max_calories = current_value
+            current_calories = []
+        else:
+            current_calories.append(int(line))
 
-  print("Solution 1: %d" % (max_calories)) 
+    print("Solution 1: %d" % (max_calories))
+
 
 def second_solution(lines: Sequence[str]) -> None:
-  current_calories: List[int] = []
-  elf_calories: List[int] = []
+    current_calories: List[int] = []
+    elf_calories: List[int] = []
 
-  for line in lines:
-    if line == '\n':
-      elf_calories.append(sum(current_calories))
-      current_calories = []
-    else:
-      current_calories.append(int(line))
-  print("Solution 2: %d" % (sum(sorted(elf_calories, reverse = True)[0:3]))) 
+    for line in lines:
+        if line == "\n":
+            elf_calories.append(sum(current_calories))
+            current_calories = []
+        else:
+            current_calories.append(int(line))
+    print("Solution 2: %d" % (sum(sorted(elf_calories, reverse=True)[0:3])))
+
 
 if __name__ == "__main__":
-  lines: List[str] = [line for line in open("input.txt")]
+    lines: List[str] = [line for line in open("input.txt")]
 
-  first_solution(lines)
-  second_solution(lines)
+    first_solution(lines)
+    second_solution(lines)
